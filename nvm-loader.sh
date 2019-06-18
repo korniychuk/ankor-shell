0380780a8111f234f18377545b036cf0afad8bc4
Anton Korniychuk <dev@korniychuk.pro>
2019-06-18 17:36:04 +0300

    - naming improved - ak.nvm.version added

--- nvm-loader.sh
+++ nvm-loader.sh
@@ -7,25 +7,19 @@
 #[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 
-export _isNvmLoaded=0
-function _ak.loadNvm() {
-  local style_Off='\033[0m'              # Text Reset
-  local style_Bold='\033[1m'
-  local style_BoldRed='\033[1;31m'
-  local style_BoldGreen='\033[1;32m'
-  local cursorToPreviousLine='\e[1A'
-  local nvmMsgPrefix='NVM Loader:'
-
-  if [[ "$_isNvmLoaded" == "0" ]]; then
-    export _isNvmLoaded=1
+export __ak_nvm_isLoaded=0
+export __ak_nvm_msgPrefix='NVM Loader:'
+function _ak.nvm.load() {
+  if [[ "$__ak_nvm_isLoaded" == "0" ]]; then
+    export __ak_nvm_isLoaded=1
 
     local command="$1"; shift
     unset -f nvm node npm npx ng
 
     if [[ "${command}" == "automatically" ]]; then
-      echo "${nvmMsgPrefix} .nvmrc found --> Loading NVM ..."
+      echo "${__ak_nvm_msgPrefix} .nvmrc found --> Loading NVM ..."
     else
-      echo "${nvmMsgPrefix} Loading ..."
+      echo "${__ak_nvm_msgPrefix} Loading ..."
     fi
 
     # This dir will contains node.js installations
@@ -41,22 +35,16 @@ function _ak.loadNvm() {
     [[ -s "$nvmShCompletion" ]] && source "$nvmShCompletion"
 
     # load .nvmrc from the current working directory in case it exists
-    local nvmRcInfo='Default'
     if [[ -f "$PWD/.nvmrc" ]]; then
       nvm use > /dev/null
-      nvmRcInfo='From .nvmrc'
+      ak.nvm.version 1
+    else
+      ak.nvm.version 0
     fi
 
     if [[ ! -f "$(which node)" ]]; then
-      echo -e "${nvmMsgPrefix} ${style_BoldRed}Can not load NodeJS${style_Off}" >&2
       return 1
     fi
-
-    echo -e "\r${cursorToPreviousLine}${nvmMsgPrefix} ${style_BoldGreen}Loaded${style_Off} --> " \
-      "node: ${style_Bold}$(node --version)${style_Off} (${nvmRcInfo})   " \
-      "npm: ${style_Bold}$(npm --version)${style_Off}   " \
-      "nvm: ${style_Bold}$(nvm --version)${style_Off}"
-#      "\n"
 
     # execute only in case at least one argument passed
     if [[ "${1}" != "" ]]; then
@@ -65,14 +53,45 @@ function _ak.loadNvm() {
   fi
 }
 
-function nvm()  { _ak.loadNvm nvm  "$@" }
-function node() { _ak.loadNvm node "$@" }
-function npm()  { _ak.loadNvm npm  "$@" }
-function npx()  { _ak.loadNvm npx  "$@" }
-function ng()   { _ak.loadNvm ng   "$@" }
-function yarn() { _ak.loadNvm yarn "$@" }
-
 # automatically loading nvm on SHELL open in case .nvmrc found
-if [[ -f "$PWD/.nvmrc" ]]; then
-  _ak.loadNvm 'automatically'
-fi
+function _ak.nvm.autoloadNvmRc() {
+  if [[ -f "$PWD/.nvmrc" ]]; then
+    _ak.nvm.load 'automatically'
+  fi
+}
+
+function ak.nvm.version() {
+  local isNvmRcUsed="${1:-unknown}"
+
+  # TODO: Use global styles function after it will be implemented
+  local style_Off='\033[0m' # Text Reset
+  local style_Bold='\033[1m'
+  local style_BoldRed='\033[1;31m'
+  local style_BoldGreen='\033[1;32m'
+  local cursorToPreviousLine='\e[1A'
+
+  local nvmRcInfo=''
+  if [[ "${isNvmRcUsed}" == "1" ]]; then
+    nvmRcInfo=' (From .nvmrc)'
+  elif [[ "${isNvmRcUsed}" == "0" ]]; then
+    nvmRcInfo=' (Default)'
+  fi
+
+  if [[ -f "$(which node)" ]]; then
+    echo -e "\r${cursorToPreviousLine}${__ak_nvm_msgPrefix} ${style_BoldGreen}Loaded${style_Off} --> " \
+      "node: ${style_Bold}$(node --version)${style_Off}${nvmRcInfo}   " \
+      "npm: ${style_Bold}$(npm --version)${style_Off}   " \
+      "nvm: ${style_Bold}$(nvm --version)${style_Off}"
+  else
+    echo -e "${__ak_nvm_msgPrefix} ${style_BoldRed}Can not load NodeJS${style_Off}" >&2
+  fi
+}
+
+function nvm()  { _ak.nvm.load nvm  "$@" }
+function node() { _ak.nvm.load node "$@" }
+function npm()  { _ak.nvm.load npm  "$@" }
+function npx()  { _ak.nvm.load npx  "$@" }
+function ng()   { _ak.nvm.load ng   "$@" }
+function yarn() { _ak.nvm.load yarn "$@" }
+
+_ak.nvm.autoloadNvmRc
