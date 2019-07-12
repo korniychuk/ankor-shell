56d6dc2adc35a069e0074f39509144776c5d5db6
Anton Korniychuk <dev@korniychuk.pro>
2019-07-12 14:26:37 +0300

    - custom-scripts improved to work with folders

--- custom-scripts.sh
+++ custom-scripts.sh
@@ -15,30 +15,60 @@ function __ak.cs.init() {
   PATH="${PATH}:${binsPath}"
 
   local -a commandsList=()
-  local fileFullPath
   local commandFullPath
-  for fileFullPath in $(find "${AK_CUSTOM_SCRIPTS_PATH}" -name "*.sh" -type f ); do
+  local fileFullPath
+  local folderFullPath
+
+  # Linking single-file commands
+  for fileFullPath in $(find "${AK_CUSTOM_SCRIPTS_PATH}" -name "*.sh" -type f -maxdepth 1 ); do
     local fileName=$(basename "${fileFullPath%.sh}")
     local commandName="aks.${fileName}"
     local commandFullPath="${binsPath}/${commandName}"
     commandsList=("${commandsList[@]}" "${commandName}")
 
-    if [[ ! -f "${commandFullPath}" ]]; then
-      echo "#!/usr/bin/env bash" > "${commandFullPath}"
-      echo "" >> "${commandFullPath}"
-      echo "source \"${AK_SCRIPT_PATH}/index.sh\"" >> "${commandFullPath}"
-      echo "source \"${fileFullPath}\" \"\${@}\"" >> "${commandFullPath}"
+    __ak.cs.checkCommandLink "${fileFullPath}" "${commandFullPath}"
+  done
 
-      chmod +x "${commandFullPath}"
+  # Linking folder commands
+  for folderFullPath in $(find "${AK_CUSTOM_SCRIPTS_PATH}" -name "*" -type d -maxdepth 1 ); do
+    local fileName=$(basename "${folderFullPath}")
+    local commandName="aks.${fileName}"
+    local commandFullPath="${binsPath}/${commandName}"
+
+    fileFullPath="${folderFullPath}/index.sh"
+    if [[ -f "${fileFullPath}" ]]; then
+      commandsList=("${commandsList[@]}" "${commandName}")
+      __ak.cs.checkCommandLink "${fileFullPath}" "${commandFullPath}"
     fi
   done
 
+  # Clear old links
   for commandFullPath in $(find "${binsPath}" -name "aks.*" -type f ); do
       if ! ak.array.inArray "$(basename "${commandFullPath}")" "${commandsList[@]}"; then
           rm -f "${commandFullPath}"
       fi
   done
-
 }
 
+function __ak.cs.checkCommandLink() {
+    local -r fileFullPath="${1}";
+    local -r commandFullPath="${2}";
+    local -r fileName=$(basename "${fileFullPath}")
+    local -r command=$(basename "${commandFullPath}")
+
+    if [[ ! -f "${commandFullPath}" ]]; then
+      echo "#!/usr/bin/env bash" > "${commandFullPath}"
+      echo "" >> "${commandFullPath}"
+      echo "declare -r AK_CS_PATH=\"${fileFullPath}\"" >> "${commandFullPath}"
+      echo 'declare -r AK_CS_DIR=$(dirname "${AK_CS_PATH}")' >> "${commandFullPath}"
+      echo "declare -r AK_CS_COMMAND=\"${command}\"" >> "${commandFullPath}"
+      echo "" >> "${commandFullPath}"
+      echo "source \"${AK_SCRIPT_PATH}/index.sh\"" >> "${commandFullPath}"
+      echo "source \"${fileFullPath}\" \"\${@}\"" >> "${commandFullPath}"
+
+
+      chmod +x "${commandFullPath}"
+    fi
+}
 __ak.cs.init
+
