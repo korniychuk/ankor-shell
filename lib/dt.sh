e76be6fa1e9c62a83d9835d7abd9eeb2f0818378
Anton Korniychuk <dev@korniychuk.pro>
2019-08-05 04:55:50 +0300

    dt.now.* functions implemented

--- lib/dt.sh
+++ lib/dt.sh
@@ -2,73 +2,54 @@
 
 #
 # Library: Date Time
+# TODO: Check GNU Date installed
 #
 
 #
-### Variables ###
+### Variables ######################################################################################
 #
 
 declare -r AK_DT_FORMAT_DATE="%Y-%m-%d"
 declare -r AK_DT_FORMAT_TIME="%H:%M:%S"
 
-declare -i __akDtGlobal_isGNUDate=-1;
+#
+# -1 - not checked
+#  0 - not GNU Date. May be BSD version from the Mac OS X
+#  1 - default `date` is GNU Date
+#  2 - default `date` it not GNU, however GNU version is available under `gdate` alias
+#
+declare -i __akDtGlobal_dateVendor=-1;
 
 #
-### Functions ###
+### Functions ######################################################################################
 #
 
-function ak.dt.normalizeDate() {
-  local -r dateStr="${1//[.\/\\-]/-}"
 
-  if ak.dt.isDateValid "${dateStr}"; then
-      echo "Invalid date" >&2
-      return 1
-  fi
-
-}
-
+# TODO: Finish
+#function ak.dt.normalizeDate() {
+#  local -r dateStr="${1//[.\/\\-]/-}"
 #
-# Check is GNU or Other(BSD for example) `date` util installed
+#  if ak.dt.isDateValid "${dateStr}"; then
+#      echo "Invalid date" >&2
+#      return 1
+#  fi
 #
-# Example:
-#
-#   if ak.dt.isGNUDate; then
-#     echo "You have GNU date installed"
-#   elif ak.dt.isGNUDatePrefixed; then
-#     echo "You have GNU date installed via `g` prefix"
-#   else
-#     echo "You don't have GNU Date installed"
-#   fi
-#
-#
-function ak.dt.isGNUDate() {
-  if [[ ${__akDtGlobal_isGNUDate} -eq -1 ]]; then
-
-    if date --version 2> /dev/null | grep GNU &> /dev/null; then
-      __akDtGlobal_isGNUDate=0
-    elif gdate --version 2> /dev/null | grep GNU &> /dev/null; then
-      __akDtGlobal_isGNUDate=1
-    else
-      __akDtGlobal_isGNUDate=2
-    fi
-  fi
-
-  return ${__akDtGlobal_isGNUDate}
-}
+#}
 
 #
 # Check is GNU Date util version installed via prefix `g`
 #
 # Example:
 #
-#   if ak.dt.isGNUDatePrefixed; then
-#     echo "You can use GNU Date with `g` prefix, like `gdate ...`"
+#   if ak.dt.hasGNUDate; then
+#     echo "You can use GNU Date via the `__ak.dt.gdate` wrapper"
+#     __ak.dt.gdate --utc "%H:%M"
 #   fi
 #
 #
-function ak.dt.isGNUDatePrefixed() {
-  ak.dt.isGNUDate
-  if [[ ${?} -eq 1 ]]; then
+function ak.dt.hasGNUDate() {
+  __ak.dt.checkDateVendor
+  if [[ ${?} -eq 1 ]] || [[ ${?} -eq 2 ]]; then
       return 0
   fi
   return 1
@@ -97,29 +78,86 @@ function ak.dt.isDateValid() {
       return 1
   fi
 
-  if ak.dt.isGNUDate; then
-      date "+${AK_DT_FORMAT_DATE}" -d "${dateStr}" &>/dev/null
-  elif ak.dt.isGNUDatePrefixed; then
-      gdate "+${AK_DT_FORMAT_DATE}" -d "${dateStr}" &>/dev/null
-  else
-      date -f "${AK_DT_FORMAT_DATE}" -j "${dateStr}" &>/dev/null
-  fi
+  __ak.dt.gdate "+${AK_DT_FORMAT_DATE}" -d "${dateStr}" &>/dev/null
 
   return $?
 }
 
-function ak.dt.getCurrentDate() {
-  date "+${AK_DT_FORMAT_DATE}"
+#
+# Print current date and time in the sortable format
+#
+# Examples:
+#
+#   ak.dt.now         # 2019-09-14 13:13:59 (GMT+3)
+#   ak.dt.now --utc   # 2019-09-14 10:13:59 (UTC)
+#
+# Notes:
+#   - "--utc" flag can be used with all ak.dt.now.* functions
+#   - you can pass any arguments of GNU Date utility. "--utc" is one of them.
+#
+function ak.dt.now() {
+  echo "$(ak.dt.now.date "${@}") $(ak.dt.now.time "${@}")"
 }
 
-function ak.dt.getCurrentTime() {
-  date "+${AK_DT_FORMAT_TIME}"
+function ak.dt.now.date() {
+  __ak.dt.gdate "+${AK_DT_FORMAT_DATE}" "${@}"
 }
 
-function ak.dt.getCurrentDateTime() {
-  echo "$(ak.dt.getCurrentDate) $(ak.dt.getCurrentTime)"
+function ak.dt.now.time() {
+  __ak.dt.gdate "+${AK_DT_FORMAT_TIME}" "${@}"
 }
 
-function ak.dt.getCurrentTimeStamp() {
-  date "+%s"
+#
+# Current timestamp
+#
+# Notes:
+#   - always in UTC (no time zone)
+#
+function ak.dt.now.timestamp() {
+  __ak.dt.gdate "+%s" "${@}"
+}
+
+#
+# Date in ISO 8601 format. Example: `2006-08-14T02:34:56-06:00`
+#
+function ak.dt.now.iso() {
+  __ak.dt.gdate --iso-8601=seconds "${@}"
+}
+
+#
+# Check is GNU or Other(BSD for example) `date` util installed
+#
+function __ak.dt.checkDateVendor() {
+  if [[ ${__akDtGlobal_dateVendor} -eq -1 ]]; then
+
+    if date --version 2> /dev/null | grep GNU &> /dev/null; then
+      __akDtGlobal_dateVendor=1
+    elif gdate --version 2> /dev/null | grep GNU &> /dev/null; then
+      __akDtGlobal_dateVendor=2
+    else
+      __akDtGlobal_dateVendor=0
+    fi
+  fi
+
+  return ${__akDtGlobal_dateVendor}
+}
+
+#
+# Universal wrapper to use `date` or `gdate` if the first one is not available.
+#
+# Example:
+#
+#     __ak.dt.gdate --utc "%H:%M"
+#
+function __ak.dt.gdate() {
+  __ak.dt.checkDateVendor
+  if [[ ${?} -eq 1 ]]; then
+      date "${@}"
+  elif [[ ${?} -eq 2 ]]; then
+      gdate "${@}"
+  else
+      echo "ERROR! Can't find GNU Date utility"
+  fi
+
+  return ${?}
 }
