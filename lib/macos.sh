cd6f6c4a3ec3bb393d92ebab594e1302e6d3f201
Anton Korniychuk <dev@korniychuk.pro>
2019-09-03 20:30:04 +0300

    chrome generator not finished

--- lib/macos.sh
+++ lib/macos.sh
@@ -5,39 +5,70 @@
 #
 
 #
-# Create separate Chrome launcher
-# https://apple.stackexchange.com/questions/66670/is-there-a-simple-way-to-have-separate-dock-icons-for-different-chrome-profiles
+# Create a separate Chrome launcher
+#
 # Notice:
 #   - Depends of perl
+#
+# Helpful links:
+#   - https://apple.stackexchange.com/questions/66670/is-there-a-simple-way-to-have-separate-dock-icons-for-different-chrome-profiles
 #
 function ak.macos.createChromeEnvironment() {
   local -r profileName="${1}"
   local -r remoteDebuggingPort="${2}"
 
+  ak.doc.heading "Chrome Environment Generator"
+  echo;
+
+  echo "Name: 'Google Chrome ${profileName}'"
+  echo -n "Remote Debugging Port: "
+  if [[ ! -z "${remoteDebuggingPort}" ]]; then
+     echo "${remoteDebuggingPort}"
+  else
+    echo "Disabled"
+  fi
+  echo;
+
+  echo "1. Automation part:"
+
   #
   # 0. Looking for the Google Chrome
   #
-  # TODO: try -r
   local chromeAppDir=$(mdfind 'kMDItemCFBundleIdentifier == "com.google.Chrome"' | head -1)
   local -r chromeBin="$chromeAppDir/Contents/MacOS/Google Chrome"
   if [[ ! -e "${chromeBin}" ]]; then
-    echo "ERROR: Can not find Google Chrome. Exiting."
-    exit 1
+    echo "ERROR: Can not find Google Chrome. Exiting." >&2
+    return 1
   fi
+  echo "  * Check Google Chrome installed - OK"
+
+  #
+  # 0. Checking name uniqueness
+  #
+  local -r customAppDir="/Applications/Google Chrome ${profileName}.app"
+  local -r profileDir="/Users/${USER}/Library/Application Support/Google/Chrome/${profileName}"
+  if [[ -d "${customAppDir}" ]]; then
+    echo "ERROR: Chrome instance with this name already exists. Exiting." >&2
+    return 2
+  fi
+  if [[ -d "${profileDir}" ]]; then
+    echo "ERROR: Chrome profile with this name already exists. Exiting." >&2
+    return 3
+  fi
+  echo "  * Checking name uniqueness - OK"
 
   #
   # 1. Directory creation
   #
-  local -r customAppDir="/Applications/Google Chrome ${profileName}.app"
   local -r wrapperDir="${customAppDir}/Contents/MacOS"
   mkdir -p "${wrapperDir}"
-  echo " * Directory created: ${wrapperDir}"
+  echo "  * Directory created: ${wrapperDir}"
 
   #
   # 2. Wrapper generation
   #
   local -r wrapperFileName="wrapper.sh"
-  local -r wrapperShFilePath="${wrapperDir}/${wrapperShFilePath}"
+  local -r wrapperShFilePath="${wrapperDir}/${wrapperFileName}"
 
   cat > "$wrapperShFilePath" <<\EOF
 #!/usr/bin/env bash
@@ -74,26 +105,66 @@ EOF
   fi
   echo "${command}\n" >> "$wrapperShFilePath"
 
-  echo " * wrapper.sh generated as ${wrapperShFilePath}"
-  if [[ ! -z "${remoteDebuggingPort}" ]]; then
-    echo " * Remote Debugging enabled on the ${remoteDebuggingPort} port"
-  fi
+  echo "  * Wrapper generated: ${wrapperShFilePath}"
 
   #
   # 3. Make the wrapper executable
   #
   chmod +x "$wrapperShFilePath"
-  echo " * Execution access added to the wrapper"
+  echo "  * Add execution permission for the wrapper - OK"
 
   #
   # 4. Copy Item.plist from the original Chrome
   #
   cp "${chromeAppDir}/Contents/Info.plist" "${customAppDir}/Contents/"
-  echo " * Info.plist copy-pasted"
-  cat "${chromeAppDir}/Contents/Info.plist" | perl -0777 -pe "s/(<key>CFBundleDisplayName<\/key>\n\s+<string>.+?)(<\/string>)/\$1 ${profileName}\$2/" | head
-  echo " * CFBundleDisplayName updated to 'Google Chrome ${profileName}'"
+#  echo "  * Info.plist copy-pasted"
+#  cat "${chromeAppDir}/Contents/Info.plist" | perl -0777 -pe "s/(<key>CFBundleDisplayName<\/key>\n\s+<string>.+?)(<\/string>)/\$1 ${profileName}\$2/" | head
+#  echo "  * CFBundleDisplayName updated to 'Google Chrome ${profileName}'"
 
 
-  echo "Done! /Applications/Google Chrome ${profileName}.app"
-  echo "You can change the app icon manually"
+  echo;
+  echo "  New Chrome Instance generated:"
+  echo "    /Applications/Google Chrome ${profileName}.app"
+  echo;
+
+  echo "2. Manual part:"
+  echo "  * You can change the app icon"
+  echo;
+
+  echo "3. To remove the new Chrome instance execute:"
+  echo "   $> ak.macos.removeChromeInstance '${profileName}'"
+  echo;
+
+  ak.doc.headingLine "Done ;)"
+}
+
+function ak.macos.removeChromeInstance() {
+  local -r profileName="${1}"
+
+  ak.doc.heading "Chrome Environment Generator"
+  echo;
+
+  local -r customAppDir="/Applications/Google Chrome ${profileName}.app"
+  local -r profileDir="/Users/${USER}/Library/Application Support/Google/Chrome/${profileName}"
+
+  if [[ -d "${customAppDir}" ]]; then
+    rm -rf "${customAppDir}"
+    if [[ $? -eq 0 ]]; then
+      echo " * Application deleted at: '${customAppDir}'"
+    fi
+  else
+      echo " * Application not found at: '${customAppDir}'"
+  fi
+
+  if [[ -d "${profileDir}" ]]; then
+    rm -rf "${profileDir}"
+    if [[ $? -eq 0 ]]; then
+      echo " * Profile deleted at: '${profileDir}'"
+    fi
+  else
+      echo " * Profile not found at: '${profileDir}'"
+  fi
+
+  echo;
+  ak.doc.headingLine "Done ;)"
 }
