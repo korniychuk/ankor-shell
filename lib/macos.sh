3e9d4067b741181c8b6ba6fe7b35014b041fa1a4
Anton Korniychuk <dev@korniychuk.pro>
2019-09-04 02:45:56 +0300

    chrome environment generator ready

--- lib/macos.sh
+++ lib/macos.sh
@@ -7,6 +7,16 @@
 #
 # Create a separate Chrome launcher
 #
+# @param {string}  profileName         suffix of the new Chrome instance
+# @param {integer} remoteDebuggingPort enable remote debugging on the port (optional)
+# @returns {void}
+#
+# @example <caption>A new Chrome instance with name 'Google Chrome Research'</caption>
+#   ak.macos.createChromeEnvironment Research
+#
+# @example <caption>A new Chrome instance with name 'Google Chrome Development' and debugging port 9222</caption>
+#   ak.macos.createChromeEnvironment Development 9222
+#
 # Notice:
 #   - Depends of perl
 #
@@ -16,6 +26,11 @@
 function ak.macos.createChromeEnvironment() {
   local -r profileName="${1}"
   local -r remoteDebuggingPort="${2}"
+
+  if [[ -z "${profileName}" ]]; then
+    echo "ERROR: Please provide the name for the new Chrome Instance" >&2
+    return 1
+  fi
 
   ak.doc.heading "Chrome Environment Generator"
   echo;
@@ -32,7 +47,12 @@ function ak.macos.createChromeEnvironment() {
   echo "1. Automation part:"
 
   #
-  # 0. Looking for the Google Chrome
+  # 1. Preparing
+  #
+  echo "  * Preparing..."
+
+  #
+  # 1.1. Looking for the Google Chrome
   #
   local chromeAppDir=$(mdfind 'kMDItemCFBundleIdentifier == "com.google.Chrome"' | head -1)
   local -r chromeBin="$chromeAppDir/Contents/MacOS/Google Chrome"
@@ -40,32 +60,38 @@ function ak.macos.createChromeEnvironment() {
     echo "ERROR: Can not find Google Chrome. Exiting." >&2
     return 1
   fi
-  echo "  * Check Google Chrome installed - OK"
+  echo "    * Checking Google Chrome installed - OK"
 
   #
-  # 0. Checking name uniqueness
+  # 1.2. Checking name uniqueness
   #
   local -r customAppDir="/Applications/Google Chrome ${profileName}.app"
   local -r profileDir="/Users/${USER}/Library/Application Support/Google/Chrome/${profileName}"
-  if [[ -d "${customAppDir}" ]]; then
+  if [[ -e "${customAppDir}" ]]; then
     echo "ERROR: Chrome instance with this name already exists. Exiting." >&2
     return 2
   fi
-  if [[ -d "${profileDir}" ]]; then
-    echo "ERROR: Chrome profile with this name already exists. Exiting." >&2
-    return 3
-  fi
-  echo "  * Checking name uniqueness - OK"
+#  if [[ -e "${profileDir}" ]]; then
+#    echo "ERROR: Chrome profile with this name already exists. Exiting." >&2
+#    return 3
+#  fi
+  echo "    * Checking name uniqueness - OK"
 
   #
-  # 1. Directory creation
+  # 2. Creating a Chrome wrapper
+  #
+  echo "  * Creating a Chrome wrapper ..."
+
+
+  #
+  # 2.1. Directory creation
   #
   local -r wrapperDir="${customAppDir}/Contents/MacOS"
   mkdir -p "${wrapperDir}"
-  echo "  * Directory created: ${wrapperDir}"
+  echo "    * Directory created: ${wrapperDir}"
 
   #
-  # 2. Wrapper generation
+  # 2.2. Wrapper generation
   #
   local -r wrapperFileName="wrapper.sh"
   local -r wrapperShFilePath="${wrapperDir}/${wrapperFileName}"
@@ -105,25 +131,51 @@ EOF
   fi
   echo "${command}\n" >> "$wrapperShFilePath"
 
-  echo "  * Wrapper generated: ${wrapperShFilePath}"
+  echo "    * Wrapper generated: ${wrapperShFilePath}"
 
   #
-  # 3. Make the wrapper executable
+  # 2.3. Make the wrapper executable
   #
   chmod +x "$wrapperShFilePath"
-  echo "  * Add execution permission for the wrapper - OK"
+  echo "    * Add execution permission for the wrapper - OK"
 
   #
-  # 4. Copy Item.plist from the original Chrome
+  # 3. Info.plist file
+  #
+  echo "  * Generating Info.plist file ..."
+
+  #
+  # 3.1. Copy Item.plist from the original Chrome
   #
   cp "${chromeAppDir}/Contents/Info.plist" "${customAppDir}/Contents/"
-#  echo "  * Info.plist copy-pasted"
-#  cat "${chromeAppDir}/Contents/Info.plist" | perl -0777 -pe "s/(<key>CFBundleDisplayName<\/key>\n\s+<string>.+?)(<\/string>)/\$1 ${profileName}\$2/" | head
-#  echo "  * CFBundleDisplayName updated to 'Google Chrome ${profileName}'"
+  echo "    * Info.plist copy-pasted from the original Google Chrome app"
 
+  #
+  # 3.2. Update Info.plist -> items
+  #
+  local -r targetItemPlistPath="${customAppDir}/Contents/Info.plist"
+
+  _ak.macos.chromeInstanceAddSuffixPlistKey "${targetItemPlistPath}" "CFBundleDisplayName" " ${profileName}"
+  echo "    * CFBundleDisplayName updated to 'Google Chrome ${profileName}'"
+
+  _ak.macos.chromeInstanceAddSuffixPlistKey "${targetItemPlistPath}" "CFBundleName" " ${profileName}"
+  echo "    * CFBundleName updated to 'Chrome ${profileName}'"
+
+  _ak.macos.chromeInstanceAddSuffixPlistKey "${targetItemPlistPath}" "CFBundleIdentifier" ".${profileName}"
+  echo "    * CFBundleIdentifier updated to 'com.google.Chrome.${profileName}'"
+
+  _ak.macos.chromeInstanceReplacePlistKey "${targetItemPlistPath}" "CFBundleExecutable" "wrapper.sh"
+  echo "    * CFBundleExecutable updated to 'wrapper.sh'"
+
+
+  #
+  # 4. Finish messages and Finder opening
+  #
+
+  ak.macos.openFinderAndSelectItem "${customAppDir}"
 
   echo;
-  echo "  New Chrome Instance generated:"
+  echo "  The new Chrome Instance is ready:"
   echo "    /Applications/Google Chrome ${profileName}.app"
   echo;
 
@@ -138,8 +190,22 @@ EOF
   ak.doc.headingLine "Done ;)"
 }
 
+#
+# Remove a separate Chrome launcher
+#
+# @param {string} profileName suffix of the new Chrome instance
+# @returns {void}
+#
+# @example <caption>A new Chrome instance with name 'Google Chrome Research'</caption>
+#   ak.macos.removeChromeEnvironment Research
+#
 function ak.macos.removeChromeInstance() {
   local -r profileName="${1}"
+
+  if [[ -z "${profileName}" ]]; then
+    echo "ERROR: Please provide the name for the new Chrome Instance" >&2
+    return 1
+  fi
 
   ak.doc.heading "Chrome Environment Generator"
   echo;
@@ -147,7 +213,7 @@ function ak.macos.removeChromeInstance() {
   local -r customAppDir="/Applications/Google Chrome ${profileName}.app"
   local -r profileDir="/Users/${USER}/Library/Application Support/Google/Chrome/${profileName}"
 
-  if [[ -d "${customAppDir}" ]]; then
+  if [[ -e "${customAppDir}" ]]; then
     rm -rf "${customAppDir}"
     if [[ $? -eq 0 ]]; then
       echo " * Application deleted at: '${customAppDir}'"
@@ -156,7 +222,7 @@ function ak.macos.removeChromeInstance() {
       echo " * Application not found at: '${customAppDir}'"
   fi
 
-  if [[ -d "${profileDir}" ]]; then
+  if [[ -e "${profileDir}" ]]; then
     rm -rf "${profileDir}"
     if [[ $? -eq 0 ]]; then
       echo " * Profile deleted at: '${profileDir}'"
@@ -167,4 +233,52 @@ function ak.macos.removeChromeInstance() {
 
   echo;
   ak.doc.headingLine "Done ;)"
+}
+
+function _ak.macos.chromeInstanceAddSuffixPlistKey() {
+  local -r filePath="${1}"
+  local -r key="${2}"
+  local -r suffix="${3}"
+
+  if [[ ! -f "${filePath}" ]]; then
+    echo "ERROR: Info.plist file is not exists or is unwritable." >&2
+    return 1
+  fi
+
+  perl -0777 -p -i -e "s/(<key>${key}<\/key>\n\s+<string>.+?)(<\/string>)/\$1${suffix}\$2/" -- "${filePath}"
+  return $?
+}
+
+function _ak.macos.chromeInstanceReplacePlistKey() {
+  local -r filePath="${1}"
+  local -r key="${2}"
+  local -r suffix="${3}"
+
+  if [[ ! -f "${filePath}" ]]; then
+    echo "ERROR: Info.plist file is not exists or is unwritable." >&2
+    return 1
+  fi
+
+  perl -0777 -p -i -e "s/(<key>${key}<\/key>\n\s+<string>).+?(<\/string>)/\$1${suffix}\$2/" -- "${filePath}"
+  return $?
+}
+
+#
+# Helpful links:
+#   - https://stackoverflow.com/questions/39214539/opening-finder-from-terminal-with-file-selected
+#
+function ak.macos.openFinderAndSelectItem() {
+  local -r itemPath="${1}"
+
+  if [[ -z "${itemPath}" ]]; then
+      echo "ERROR: Please specify item path. Can not open Finder." >&2
+      return 1
+  fi
+  if [[ ! -e "${itemPath}" ]]; then
+      echo "ERROR: The item is not exists. Can not open Finder. (${itemPath})" >&2
+  fi
+
+  osascript -e "tell application \"Finder\"" -e activate -e "reveal POSIX file \"${itemPath}\"" -e end tell
+
+  return 0
 }
