e48e7f196e464379a5da24267b4b710ea307795d
Anton Korniychuk <dev@korniychuk.pro>
2019-09-08 12:36:17 +0300

    todo

--- lib/bash.sh
+++ lib/bash.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# TODO: Split bash to shell + bash/zsh
+
 #
 # TODO: Implement normalize boolean function '', 0, false, null
 #   - https://unix.stackexchange.com/questions/185670/what-is-a-best-practice-to-represent-a-boolean-value-in-a-shell-script
