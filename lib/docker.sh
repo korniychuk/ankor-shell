dd8b510d0720fb84589529aeeefc34c2f123ddcf
Anton Korniychuk <dev@korniychuk.pro>
2019-09-16 19:35:58 +0300

    - ak.docker simple refactoring

--- lib/docker.sh
+++ lib/docker.sh
@@ -27,7 +27,7 @@ HELP
   declare tags=$(wget -q https://registry.hub.docker.com/v1/repositories/${image}/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}')
 
   if [[ -n "${2}" ]]; then
-      tags=` echo "${tags}" | grep "$2" `
+    tags=$(echo "${tags}" | grep "$2")
   fi
 
   echo "${tags}"
