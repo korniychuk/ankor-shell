fef9664cb2dfd21aefc779de2194c44f33d23d10
Anton Korniychuk <dev@korniychuk.pro>
2019-12-26 17:16:15 +0200

    - fix problems after refactoring

--- lib/bash.sh
+++ lib/bash.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# TODO: Split bash to shell + bash/zsh
-
-#
-# TODO: Implement normalize boolean function '', 0, false, null
-#   - https://unix.stackexchange.com/questions/185670/what-is-a-best-practice-to-represent-a-boolean-value-in-a-shell-script
-#   - https://www.google.com/search?q=bash+falsy+values&oq=bash+falsy+values&aqs=chrome..69i57j0.4094j0j4&sourceid=chrome&ie=UTF-8
-#
-
 #
 # Example:
 #
