d5a997d758f5f0c2f935d64cb669156abc938521
Anton Korniychuk <dev@korniychuk.pro>
2021-02-19 04:27:47 +0200

    Disk Aliases improved has been improved to support base path

--- disk-aliases.sh
+++ disk-aliases.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+# AK_DA <= AnKor DiskAliases
+declare -r _AK_DA_ROOT_PREFIX="${1:-''}"
+
+export -r AK_DA_CODE="${_AK_DA_ROOT_PREFIX}/code"
+export -r AK_DA_PROJECTS="${_AK_DA_ROOT_PREFIX}/projects"
+export -r AK_DA_STORAGE="${_AK_DA_ROOT_PREFIX}/storage"
+export -r AK_DA_INFO="${_AK_DA_ROOT_PREFIX}/info"
+export -r AK_DA_MEGA_CLOUD="${_AK_DA_ROOT_PREFIX}/mega-cloud"
+
+#
 # Custom Aliases
-alias cdc="cd /code"
-alias cdp="cd /projects"
-alias cds="cd /storage"
-alias cdi="cd /info"
-alias cdm="cd /mega-cloud"
+#
+alias cdc='cd "${AK_DA_CODE}"'
+alias cdp='cd "${AK_DA_PROJECTS}"'
+alias cds='cd "${AK_DA_STORAGE}"'
+alias cdi='cd "${AK_DA_INFO}"'
+alias cdm='cd "${AK_DA_MEGA_CLOUD}"'
